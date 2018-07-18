1f6bfd1ffd8e2389dae1519a185b77dfd17918fc
Adam Grandquist <grandquista@gmail.com>
2018-07-18 16:44:51 -0700

    Initial Commit

---
+++ sudoku/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sudoku
+//
+//  Created by Adam Grandquist on 7/18/18.
+//  Copyright © 2018 Adam Grandquist. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
